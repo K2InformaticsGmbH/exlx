ce8fa87efa37f1af868c3c9fc998585efeaf0a54
Bikram Chatterjee <razorpeak@gmail.com>
2016-02-10 10:34:40 +0100

    code migrated

---
+++ src/xlsx.hrl
@@ -0,0 +1,80 @@
+-ifndef(_XLSX_HRL_).
+-define(_XLSX_HRL_, true).
+
+-define(XMLTAG, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>").
+
+-define(FMTSCHEMA_URL_PREFIX, "http://schemas.openxmlformats.org").
+-define(OXML_PKG, ?FMTSCHEMA_URL_PREFIX"/package/2006").
+-define(OXML_SST, ?FMTSCHEMA_URL_PREFIX"/spreadsheetml/2006/main").
+-define(OXML_ODR, ?FMTSCHEMA_URL_PREFIX"/officeDocument/2006/relationships").
+-define(OXML_MRC, ?FMTSCHEMA_URL_PREFIX"/markup-compatibility/2006").
+
+-define(OXML_CT, "application/vnd.openxmlformats").
+-define(OXML_CT_REL, ?OXML_CT"-package.relationships+xml").
+-define(OXML_CT_SSX, ?OXML_CT"-officedocument.spreadsheetml.sheet.main+xml").
+-define(OXML_CT_WSX, ?OXML_CT"-officedocument.spreadsheetml.worksheet+xml").
+-define(OXML_CT_STL, ?OXML_CT"-officedocument.spreadsheetml.styles+xml").
+
+-define(CONTENT_TYPES_PATH, "[Content_Types].xml").
+-define(CONTENT_TYPES_BIN,
+        <<?XMLTAG
+"<Types xmlns=\""?OXML_PKG"/content-types\">"
+    "<Default Extension=\"rels\" ContentType=\""?OXML_CT_REL"\"/>"
+    "<Default Extension=\"xml\" ContentType=\"application/xml\"/>"
+    "<Override PartName=\"/workbook.xml\" ContentType=\""?OXML_CT_SSX"\"/>"
+    "<Override PartName=\"/worksheet.xml\" ContentType=\""?OXML_CT_WSX"\"/>"
+    "<Override PartName=\"/styles.xml\" ContentType=\""?OXML_CT_STL"\"/>"
+"</Types>"
+        >>).
+
+-define(DOT_RELS_PATH, "_rels/.rels").
+-define(DOT_RELS_BIN,
+        <<?XMLTAG
+"<Relationships xmlns=\""?OXML_PKG"/relationships\">"
+    "<Relationship Id=\"rId1\" Type=\""?OXML_ODR"/officeDocument\" "
+                  "Target=\"workbook.xml\"/>"
+"</Relationships>"
+        >>).
+
+-define(WORKBOOK_PATH, "workbook.xml").
+-define(WORKBOOK_BIN(__SheetName),
+        <<?XMLTAG
+"<workbook xmlns=\""?OXML_SST"\" xmlns:r=\""?OXML_ODR"\">"
+    "<sheets><sheet name=\"",__SheetName/binary,"\" "
+                   "sheetId=\"1\" r:id=\"rId1\"/></sheets>"
+"</workbook>"
+        >>).
+
+-define(WORKBOOK_RELS_PATH, "_rels/workbook.xml.rels").
+-define(WORKBOOK_RELS_BIN,
+        <<?XMLTAG
+"<Relationships xmlns=\""?OXML_PKG"/relationships\">"
+    "<Relationship Id=\"rId1\" Type=\""?OXML_ODR"/worksheet\" "
+                  "Target=\"worksheet.xml\"/>"
+    "<Relationship Id=\"rId2\" Type=\""?OXML_ODR"/styles\" "
+                  "Target=\"styles.xml\"/>"
+"</Relationships>"
+        >>).
+
+
+-define(WORKSHEET_PATH, "worksheet.xml").
+-define(WORKSHEET_BIN(__SheetData),
+        <<?XMLTAG
+"<worksheet xmlns=\""?OXML_SST"\" xmlns:r=\""?OXML_ODR"\" xmlns:mc=\""?OXML_MRC"\">"
+    "<sheetViews><sheetView workbookViewId=\"0\"/></sheetViews>"
+    "<sheetFormatPr baseColWidth=\"10\" defaultColWidth=\"22.7109375\" defaultRowHeight=\"15\"/>"
+    "<sheetData>",
+        __SheetData/binary,
+    "</sheetData>"
+"</worksheet>"
+        >>).
+
+-define(STYLES_PATH, "styles.xml").
+-define(STYLES_BIN(__StyleData),
+        <<?XMLTAG
+"<styleSheet xmlns=\""?OXML_SST"\">",
+    __StyleData/binary,
+"</styleSheet>"
+        >>).
+
+-endif. %% _XLSX_HRL_
---
+++ src/xlsx_writer.erl
@@ -0,0 +1,178 @@
+-module(xlsx_writer).
+-include("xlsx.hrl").
+
+-export([create_xlsx/4]).
+
+% @doc
+% * ColumnID can only be one/two uppersace letters (^[A-Z]{1,2}$)
+-spec create_xlsx(File :: file:name(),
+                  SheetTitle :: binary(),
+                  Sheet :: #{RowId :: integer() =>
+                             #{ColumnId :: list()|atom()|binary() =>
+                               Value :: list()|binary()|integer()|float()|map()}
+                            },
+                  Style :: #{Fonts :: fonts => any()}) ->
+%                  Style :: #{Fonts :: fonts|<<"fonts">> => [#{name|<<"name">> => list()|binary(),
+%                                                     color|<<"color">> => list()|binary(),
+%                                                     size|<<"size">> => float(),
+%                                                     bold|<<"bold">> => true|false,
+%                                                     italics|<<"italics">> => true|false,
+%                                                     underline|<<"underline">> => true|false,
+%                                                     strike|<<"strike">> => true|false}],
+%                            fills|<<"fills">> => [#{type|<<"type">> => list()|binary(),
+%                                                    fg|<<"fg">> => list()|binary(),
+%                                                    bg|<<"bg">> => list()|binary()}],
+%                            xfs|<<"xfs">> => [#{fill|<<"fill">> => integer(),
+%                                                font|<<"font">> => integer()}]}) ->
+    {ok, binary()} | {error, term()}.
+create_xlsx(File, SheetTitle, Sheet, Style)
+  when is_binary(SheetTitle), is_map(Sheet), is_map(Style) ->
+    try
+        SheetData = to_sheet_data(Sheet),
+        StyleData = to_style_data(Style),
+        case zip:zip(
+               File,
+               [{?CONTENT_TYPES_PATH, ?CONTENT_TYPES_BIN},
+                {?DOT_RELS_PATH, ?DOT_RELS_BIN},
+                {?WORKBOOK_RELS_PATH, ?WORKBOOK_RELS_BIN},
+                {?STYLES_PATH, ?STYLES_BIN(StyleData)},
+                {?WORKBOOK_PATH, ?WORKBOOK_BIN(SheetTitle)},
+                {?WORKSHEET_PATH, ?WORKSHEET_BIN(SheetData)}
+               ], [memory]) of
+            {ok, {File, FileContent}} -> {ok, FileContent};
+            {error, Reason} -> {error, Reason}
+        end
+    catch
+        error:E -> {error,E}
+    end.
+
+-spec to_sheet_data(map()) -> binary().
+to_sheet_data(Sheet) when is_map(Sheet) ->
+    lists:foldl(
+      fun(RowId, XML) ->
+              RowIdBin = integer_to_binary(RowId),
+              Row = maps:get(RowId, Sheet),
+              <<XML/binary, "<row r=\"",RowIdBin/binary,"\">",
+                (lists:foldl(
+                   fun(Col, ColXML) ->
+                           ColBin = if is_atom(Col) -> atom_to_binary(Col, utf8);
+                                       is_list(Col) -> list_to_binary(Col);
+                                       is_binary(Col) -> Col;
+                                       true -> error({badcolumn, Col})
+                                    end,
+                           case re:run(ColBin,"^[A-Z]{1,2}$") /= nomatch of
+                               true -> ok;
+                               _ -> error({badcolumnid, Col})
+                           end,
+                           Val = maps:get(Col,Row),
+                           {V, Stl} = case Val of
+                                          #{style := S, data := D} -> {D, S+1};
+                                          #{<<"style">> := S, <<"data">> := D} -> {D, S+1};
+                                          D -> {D, 0}
+                                      end,
+                           <<ColXML/binary,
+                             "<c s=\"",(integer_to_binary(Stl))/binary,"\" r=\"",ColBin/binary,RowIdBin/binary,"\"",
+                             (case V of
+                                 V when is_binary(V) ->
+                                     <<" t=\"inlineStr\"><is><t>", V/binary, "</t></is>">>;
+                                 V when is_list(V) ->
+                                     <<" t=\"inlineStr\"><is><t>", (list_to_binary(V))/binary, "</t></is>">>;
+                                 V when is_integer(V) -> <<"><v>", (integer_to_binary(V))/binary, "</v>">>;
+                                 V when is_float(V) -> <<"><v>", (float_to_binary(V))/binary, "</v>">>;
+                                 V -> error({badvalue, V})
+                              end)/binary,
+                             "</c>">>
+                   end, <<>>,
+                   lists:usort(maps:keys(Row))))/binary,
+                "</row>">>
+      end, <<>>, lists:usort(maps:keys(Sheet))).
+
+-define(MAPSGET(__K,__M,__D), maps:get(<<??__K>>, __M, maps:get(??__K, __M, maps:get(__K, __M, __D)))).
+to_style_data(Style) ->
+    FontBins = lists:foldl(
+                 fun(Font, Fonts) when is_map(Font) ->
+                         [<<"<font>",
+                            (list_to_binary(
+                               [case ?MAPSGET(bold,Font,false) == true of
+                                    true -> "<b/>";
+                                    _ -> ""
+                                end,
+                                case ?MAPSGET(italics,Font,false) == true of
+                                    true -> "<i/>";
+                                    _ -> ""
+                                end,
+                                case ?MAPSGET(underline,Font,false) == true of
+                                    true -> "<u/>";
+                                    _ -> ""
+                                end,
+                                case ?MAPSGET(strike,Font,false) == true of
+                                    true -> "<strike/>";
+                                    _ -> ""
+                                end,
+                                case ?MAPSGET(size,Font,none) of
+                                    none -> "";
+                                    Size -> ["<sz val=\"",float_to_list(Size,[{decimals,2},compact]),"\"/>"]
+                                end,
+                                case ?MAPSGET(color,Font,none) of
+                                    none -> "";
+                                    Color -> ["<color rgb=\"",Color,"\"/>"]
+                                end,
+                                case ?MAPSGET(name,Font,none) of
+                                    none -> "";
+                                    Name -> ["<name val=\"",Name,"\"/>"]
+                                end]))/binary,
+                            "</font>">> | Fonts]
+                 end,
+                 [], ?MAPSGET(fonts, Style, [])),
+    FontsCount = integer_to_binary(length(FontBins)+1),
+    FillBins = lists:foldl(
+                 fun(Fill, Fills) when is_map(Fill) ->
+                         [<<"<fill><patternFill patternType=\"",
+                            (list_to_binary(
+                               [case ?MAPSGET(type,Fill,none) of
+                                    none -> "solid";
+                                    Type -> Type
+                                end,"\"><fgColor rgb=\"",
+                                case ?MAPSGET(fg,Fill,none) of
+                                    none -> "FFFFFFFF";
+                                    FgCol -> FgCol
+                                end,"\"/><bgColor rgb=\"",
+                                case ?MAPSGET(bg,Fill,none) of
+                                    none -> "FFFFFFFF";
+                                    FgCol -> FgCol
+                                end,"\"/>"]))/binary,
+                            "</patternFill></fill>">> | Fills]
+                 end,
+                 [], ?MAPSGET(fills,Style,[])),
+    FillsCount = integer_to_binary(length(FillBins)+2),
+    XfsBins = lists:foldl(
+                 fun(Xf, Xfs) when is_map(Xf) ->
+                         [<<"<xf fillId=\"",
+                            (list_to_binary(
+                               [case ?MAPSGET(fill,Xf,none) of
+                                    none -> "0";
+                                    FillId -> integer_to_list(FillId+2)
+                                end,"\" fontId=\"",
+                                case ?MAPSGET(font,Xf,none) of
+                                    none -> "0";
+                                    FontId -> integer_to_list(FontId+1)
+                                end,"\""]))/binary,
+                            " xfId=\"0\" applyFill=\"1\"/>">> | Xfs]
+                 end,
+                 [], ?MAPSGET(xfs,Style,[])),
+    XfsCount = integer_to_binary(length(XfsBins)+1),
+    
+    <<"<fonts count=\"",FontsCount/binary,"\"><font/>",
+      (list_to_binary(lists:reverse(FontBins)))/binary,"</fonts>"
+
+      "<fills count=\"",FillsCount/binary,"\"><fill/>"
+      "<fill><patternFill patternType=\"lightGray\"/></fill>",
+      (list_to_binary(lists:reverse(FillBins)))/binary,
+      "</fills>"
+
+      "<borders count=\"1\"><border/></borders>"
+      "<cellStyleXfs count=\"1\"><xf/></cellStyleXfs>"
+
+      "<cellXfs count=\"",XfsCount/binary,"\"><xf/>",
+      (list_to_binary(lists:reverse(XfsBins)))/binary,
+      "</cellXfs>">>.
